8e907dfa0dc812a579ae12859b91ae595b6b4f5d
2 am Quang <86760575+MinhQuang03@users.noreply.github.com>
2023-10-29 22:54:43 +0700

    add-123-quang

--- AppData/IServices/IPhoneDetailService.cs
+++ AppData/IServices/IPhoneDetailService.cs
@@ -15,5 +15,25 @@ namespace AppData.IServices
         public Task<List<PhoneDetaild>> GetPhoneDetailds(Guid IdPhone);
 
         Task<PhoneDetaild> GetById(Guid id);
+
+        Task<PhoneDetaild> GetPhoneByColor(Guid IdColor);
+
+        Task<PhoneDetaild> GetPhoneByRam(Guid IdRam);
+
+        Task<PhoneDetaild> GetPhoneByChipCPUs(Guid IdChipCPUs);
+
+        Task<List<PhoneDetaild>> GetPhoneDESC();
+
+        Task<List<PhoneDetaild>> GetPhoneASC();
+
+        Task<List<PhoneDetaild>> GetPhone5tr();
+
+        Task<List<PhoneDetaild>> GetPhone10tr();
+
+        Task<List<PhoneDetaild>> GetPhone15tr();
+
+        Task<List<PhoneDetaild>> GetPhone25tr();
+
+        Task<List<PhoneDetaild>> GetPhone50tr();
     }
 }
