e6e75b750f4d3d53cb663f15741b6ae9562a09e2
thanh05012003 <92389586+thanh05012003@users.noreply.github.com>
2023-11-13 21:59:23 +0700

    add view chon tinh thanh pho

--- PRO219_WebsiteBanDienThoai_FPhone/Controllers/CheckoutController.cs
+++ PRO219_WebsiteBanDienThoai_FPhone/Controllers/CheckoutController.cs
@@ -1,12 +1,38 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using PRO219_WebsiteBanDienThoai_FPhone.ViewModel;
+using PRO219_WebsiteBanDienThoai_FPhone.ViewModel.NewFolder;
+using PRO219_WebsiteBanDienThoai_FPhone.ViewModel.Provinces;
 
 namespace PRO219_WebsiteBanDienThoai_FPhone.Controllers
 {
     public class CheckoutController : Controller
     {
-        public IActionResult Index()
+	    private HttpClient _client;
+     
+	    public CheckoutController(HttpClient client)
+	    {
+		    _client = client;
+            _client.DefaultRequestHeaders.Add("token", "a799ced2-febc-11ed-a967-deea53ba3605");
+        }
+	    public async Task<IActionResult> Index()
         {
-            return View();
+            CheckOutViewModel model = new CheckOutViewModel();
+            var data = await (await _client.GetAsync("https://online-gateway.ghn.vn/shiip/public-api/master-data/province")).Content.ReadAsStringAsync();
+            if (!string.IsNullOrEmpty(data))
+            {
+                var Province = JsonConvert.DeserializeObject<ApiResponse<Province>>(data);
+                model.Provinces = Province.Data;
+            }
+            return View(model);
+        }
+
+        public async Task<JsonResult> GetProvince()
+        {
+            _client.DefaultRequestHeaders.Add("token", "a799ced2-febc-11ed-a967-deea53ba3605");
+	       var data = await _client.GetAsync("https://online-gateway.ghn.vn/shiip/public-api/master-data/province");
+	       var name =  data.Content.ReadAsStringAsync();
+           return Json(name);
         }
     }
 }
---
+++ PRO219_WebsiteBanDienThoai_FPhone/ViewModel/CheckOutViewModel.cs
@@ -0,0 +1,9 @@
+﻿using PRO219_WebsiteBanDienThoai_FPhone.ViewModel.NewFolder;
+
+namespace PRO219_WebsiteBanDienThoai_FPhone.ViewModel
+{
+    public class CheckOutViewModel
+    {
+        public List<Province> Provinces { get; set; } = new List<Province>();
+    }
+}
