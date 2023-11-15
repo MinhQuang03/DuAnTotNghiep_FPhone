e1ed6e46b56eb7168403729723c8bfd91335f222
thanh05012003 <92389586+thanh05012003@users.noreply.github.com>
2023-11-15 20:13:32 +0700

    lay thon tin san pham ra form thanh toan

--- AppData/FPhoneDbContexts/FPhoneDbContext.cs
+++ AppData/FPhoneDbContexts/FPhoneDbContext.cs
@@ -59,7 +59,7 @@ public class FPhoneDbContext : IdentityDbContext<ApplicationUser>
     // Ko cần sửa file này
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        optionsBuilder.UseSqlServer(@"Data Source=fphone.database.windows.net;Initial Catalog=PRO219_WebsiteBanDienThoai;User ID=thanhnx;Password=123456aA@");
+        optionsBuilder.UseSqlServer(@"Data Source=MSI;Initial Catalog=PRO219_WebsiteBanDienThoai;Integrated Security=True");
     }
     protected override void OnModelCreating(ModelBuilder builder)
     {
--- PRO219_WebsiteBanDienThoai_FPhone/Controllers/CheckoutController.cs
+++ PRO219_WebsiteBanDienThoai_FPhone/Controllers/CheckoutController.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AppData.IServices;
+using AppData.Models;
+using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using PRO219_WebsiteBanDienThoai_FPhone.Models;
+using PRO219_WebsiteBanDienThoai_FPhone.Services;
 using PRO219_WebsiteBanDienThoai_FPhone.ViewModel;
 using PRO219_WebsiteBanDienThoai_FPhone.ViewModel.NewFolder;
 using PRO219_WebsiteBanDienThoai_FPhone.ViewModel.Provinces;
@@ -9,21 +13,59 @@ namespace PRO219_WebsiteBanDienThoai_FPhone.Controllers
     public class CheckoutController : Controller
     {
 	    private HttpClient _client;
+        private IVwPhoneDetailService _service;
      
-	    public CheckoutController(HttpClient client)
+	    public CheckoutController(HttpClient client,IVwPhoneDetailService service)
 	    {
 		    _client = client;
             _client.DefaultRequestHeaders.Add("token", "a799ced2-febc-11ed-a967-deea53ba3605");
+            _service = service;
         }
 	    public async Task<IActionResult> Index()
         {
+            var userId = User.Claims.FirstOrDefault(claim => claim.Type == "Id")?.Value;
             CheckOutViewModel model = new CheckOutViewModel();
+            //lấy dữ liệu tỉnh thành phố
             var data = await (await _client.GetAsync("https://online-gateway.ghn.vn/shiip/public-api/master-data/province")).Content.ReadAsStringAsync();
-            if (!string.IsNullOrEmpty(data))
-            {
-                var Province = JsonConvert.DeserializeObject<ApiResponse<Province>>(data);
+             var Province = JsonConvert.DeserializeObject<ApiResponse<Province>>(data);
                 model.Provinces = Province.Data;
-            }
+
+                //kiểm tra đăng nhập
+                if (userId == null) //Chưa đăng nhập
+                {
+                    //lấy thông tin giỏ hàng từ session ( chưa đăng nhập )
+                    var datas = SessionService<CartDetailModel>.GetObjFromSession(HttpContext.Session, "Cart");
+                    // kiểm tra nếu thông tin trong session đúng với trong db thì gán giá trị cho lstcartdetail
+                    foreach (var item in datas)
+                    {
+                        if (_service.CheckPhoneDetail(item.phoneDetaild.Id) >= 1)
+                        {
+                            model.lstCartDetail.Add(_service.getPhoneDetailByIdPhoneDetail(item.phoneDetaild.Id));
+                        }
+                    }
+                }
+                
+                if (string.IsNullOrEmpty(userId)) //Đã đăng nhập
+                {
+                    
+                }
+
+                return View(model);
+        }
+        [HttpPost]
+        public IActionResult Index(CheckOutViewModel model)
+        {
+            // lấy data từ model gán vào bill
+            model.Bill = new Bill()
+            {
+                Id = Guid.NewGuid(),
+                Address = model.DetailedAddress + ", " + model.Ward + "/" + model.District + "/" + model.Province,
+                PaymentDate = DateTime.Now,
+                Name = model.Name,
+                Status = 0,
+                Phone = model.Phone
+            };
+
             return View(model);
         }
 
