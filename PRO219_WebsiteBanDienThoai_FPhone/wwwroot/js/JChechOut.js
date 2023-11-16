cc187e8b98cd215e7e00e5f06d34ce2a2a74c887
thanh05012003 <92389586+thanh05012003@users.noreply.github.com>
2023-11-16 20:34:50 +0700

    n

--- PRO219_WebsiteBanDienThoai_FPhone/wwwroot/js/JChechOut.js
+++ PRO219_WebsiteBanDienThoai_FPhone/wwwroot/js/JChechOut.js
@@ -34,7 +34,7 @@
                             }</option>`;
                     }
                     $("#District").html(stringHtml);
-                    $("#ProvinceName").val($("#Province option:selected").text());  
+                    $("#ProvinceName").val($("#Province option:selected").text());
                 }
             });
         } else {
@@ -54,7 +54,7 @@
             const headers = {
                 token: "a799ced2-febc-11ed-a967-deea53ba3605"
             };
-           var data = {
+            const data = {
                 district_id: value
             };
             $.ajax({
@@ -63,7 +63,7 @@
                 headers: headers,
                 data: data,
                 success: (data) => {
-                  
+
                     stringHtml = '<option value=" " > --Lựa chọn-- </option>';
                     for (let i = 0; i < data.data.length; i++) {
                         stringHtml += `<option value="${data.data[i].WardCode}" >${data.data[i].WardName}</option>`;
@@ -80,23 +80,22 @@
 
     };
 
-    ins.ChangeWard = function ()
-    {
+    ins.ChangeWard = function() {
         const value = $("#Ward").val();
-        if (value.length>1){
+        if (value.length > 1) {
             $("#WardName").val($("#Ward option:selected").text());
         } else {
             $("#WardName").empty();
         }
-    }
+    };
     //Lấy gói dịch vụ
-    ins.AvailableService = function (toDistrict) {
-        var url = "https://online-gateway.ghn.vn/shiip/public-api/v2/shipping-order/available-services";
-        let stringHtml = '';
+    ins.AvailableService = function(toDistrict) {
+        const url = "https://online-gateway.ghn.vn/shiip/public-api/v2/shipping-order/available-services";
+        let stringHtml = "";
         const header = {
             token: "a799ced2-febc-11ed-a967-deea53ba3605"
         };
-        var data = {
+        const data = {
             shop_id: shopId,
             from_district: fromDistrict,
             to_district: toDistrict
@@ -114,38 +113,38 @@
                 $("#AvailableService").html(stringHtml);
             }
         });
-    }
+    };
 
     //Tính phí ship
     ins.TotalShip = function() {
-        var wardValue = $("#Ward").val(); // lấy code xã/phường
-        var districtValue = $("#District").val(); // lấy code quận/huyện
-        var sumPhone = $("#SumPhone").val(); //số lượng sảnphẩm
-        var insurance = $("#TotalPhone").val(); // tổng tiền sản phẩm
-        var serviceValue = $("#AvailableService").val(); //Thông tin gói dịch vụ
-        var weight = 100; // trọng lượng  (gram)
-        var length = 20; // chiều dài
-        var width = 10; //chiều rộng
-        var height = 3; // chiều cao
+        const wardValue = $("#Ward").val(); // lấy code xã/phường
+        const districtValue = $("#District").val(); // lấy code quận/huyện
+        const sumPhone = $("#SumPhone").val(); //số lượng sảnphẩm
+        const insurance = $("#TotalPhone").val(); // tổng tiền sản phẩm
+        const serviceValue = $("#AvailableService").val(); //Thông tin gói dịch vụ
+        const weight = 100; // trọng lượng  (gram)
+        const length = 20; // chiều dài
+        const width = 10; //chiều rộng
+        const height = 3; // chiều cao
 
-        const url = "https://online-gateway.ghn.vn/shiip/public-api/v2/shipping-order/fee";
         const header = {
             token: "a799ced2-febc-11ed-a967-deea53ba3605",
             shop_id: shopId
         };
-       var data = {
-           service_id: serviceValue, 
-           insurance_value: insurance,
-           coupon: "",
-           to_ward_code: wardValue,
-           to_district_id: districtValue,
-           from_district_id: fromDistrict, 
-           weight: weight,
-           length: length,
-           width: width,
-           height: height
-       };
+        const data = {
+            service_id: serviceValue,
+            insurance_value: insurance,
+            coupon: "",
+            to_ward_code: wardValue,
+            to_district_id: districtValue,
+            from_district_id: fromDistrict,
+            weight: weight,
+            length: length,
+            width: width,
+            height: height
+        };
         if (wardValue, districtValue, sumPhone, insurance, serviceValue) {
+            const url = "https://online-gateway.ghn.vn/shiip/public-api/v2/shipping-order/fee";
             $.ajax({
                 method: "GET",
                 url: url,
@@ -157,26 +156,26 @@
                 error: (error) => {
                     $("#TotalShip").empty();
                     Swal.fire({
-                        icon: 'error',
-                        title: 'Có lỗi xảy ra!',
-                        text: 'Vui lòng chọn phương thức vận chuyển khác',
+                        icon: "error",
+                        title: "Có lỗi xảy ra!",
+                        text: "Vui lòng chọn phương thức vận chuyển khác",
                         allowOutsideClick: false,
                     });
                 }
             });
         }
-       
-    }
+
+    };
 
     //khi chọn gói dịch vụ sẽ tính phí ship
-    ins.ChangeService = function () {
-       var value= $("#AvailableService").val();
-        if (value.length>1) {
+    ins.ChangeService = function() {
+        const value = $("#AvailableService").val();
+        if (value.length > 1) {
             ins.TotalShip();
         } else {
             $("#TotalShip").empty();
         }
-    }
+    };
 
     return ins;
 })(window, jQuery);
