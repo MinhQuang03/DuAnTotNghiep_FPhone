b0344e7318d353450d3a9bae7ab8490ce25ba2f4
KhanhLK99 <106392405+KhanhLK99@users.noreply.github.com>
2023-11-22 22:40:15 +0700

    bill

--- AppData/Models/Bill.cs
+++ AppData/Models/Bill.cs
@@ -8,6 +8,7 @@
 
         public DateTime PaymentDate { get; set; }
 
+     
         public string Name { get; set; }
 
         public string Phone { get; set; }
@@ -16,7 +17,7 @@
 
         public int Status { get; set; }
         public string? BillCode { get; set; }
-        public decimal TotalMoney { get; set; }
+        public decimal? TotalMoney { get; set; }
 
         public int StatusPayment { get; set; }
 
