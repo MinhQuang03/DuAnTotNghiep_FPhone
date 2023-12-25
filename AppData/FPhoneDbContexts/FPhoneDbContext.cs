b3c5a573c2f11dc5f48fe6fd8c5d20d8fb0a315e
2 am Quang <86760575+MinhQuang03@users.noreply.github.com>
2023-12-25 22:35:50 +0700

    1233

--- AppData/FPhoneDbContexts/FPhoneDbContext.cs
+++ AppData/FPhoneDbContexts/FPhoneDbContext.cs
@@ -59,7 +59,7 @@ public class FPhoneDbContext : IdentityDbContext<ApplicationUser>
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
 
-        optionsBuilder.UseSqlServer(@"Data Source=LKK\SQLEXPRESS ;Initial Catalog=PRO219_WebsiteBanDienThoai ;Integrated Security=True");
+        optionsBuilder.UseSqlServer(@"Data Source=MSI\SQLEXPRESS ;Initial Catalog=PRO219_WebsiteBanDienThoai ;Integrated Security=True");
 
     }
     protected override void OnModelCreating(ModelBuilder builder)
