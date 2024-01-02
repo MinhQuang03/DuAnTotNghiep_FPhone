9dbc97669181577a551990ac628ef3ad3f6e234e
thanh05012003 <92389586+thanh05012003@users.noreply.github.com>
2024-01-03 00:47:19 +0700

    Thêm phần quản lý nội dung( blog )

--- AppData/Models/Blog.cs
+++ AppData/Models/Blog.cs
@@ -5,8 +5,9 @@
         public Guid Id { get; set; }
 
         public string? Title { get; set; }
+        public string IntroText { get; set; }
 
-        public string? Content { get; set; }
+        public string? Content { get; set; }    
 
         public DateTime? CreatedDate { get; set; }
 
