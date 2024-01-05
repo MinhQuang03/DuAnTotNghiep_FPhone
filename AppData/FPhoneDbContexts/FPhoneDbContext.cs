b3ff5ddaf9d131dce1247cea05dc4e4f57714c1c
Nguyễn ThànhMinh <minhnguyen16082001@gmail.com>
2024-01-05 21:51:01 +0700

    commit view

--- AppData/FPhoneDbContexts/FPhoneDbContext.cs
+++ AppData/FPhoneDbContexts/FPhoneDbContext.cs
@@ -59,7 +59,7 @@ public class FPhoneDbContext : IdentityDbContext<ApplicationUser>
     // Ko cần sửa file này
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        optionsBuilder.UseSqlServer(@"Data Source=.;Initial Catalog=PRO219_WebsiteBanDienThoai ;Integrated Security=True");
+        optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-GEO6VF4;Initial Catalog=PRO219_WebsiteBanDienThoai ;Integrated Security=True");
     }
     protected override void OnModelCreating(ModelBuilder builder)
     {
