f19509b9b998f2e14c6f949ae3ef59ce823d9de3
thanh05012003 <92389586+thanh05012003@users.noreply.github.com>
2024-01-11 16:41:06 +0700

    Đổi form đăng nhập

--- PRO219_WebsiteBanDienThoai_FPhone/Controllers/AccountsController.cs
+++ PRO219_WebsiteBanDienThoai_FPhone/Controllers/AccountsController.cs
@@ -119,6 +119,11 @@ public class AccountsController : Controller
         return View(model);
     }
 
+    public IActionResult Login()
+    {
+        return View();
+    }
+
     [HttpPost]
     public async Task<IActionResult> Login(LoginModel model)
     {
