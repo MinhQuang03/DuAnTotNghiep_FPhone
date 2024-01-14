6c9f89502da0c6329ffd4061ac9b247019a4b538
Nguyễn ThànhMinh <minhnguyen16082001@gmail.com>
2024-01-14 15:46:33 +0700

    Update Ranking

---
+++ QuerySQL/vTop5Update.sql
@@ -0,0 +1,16 @@
+Create or alter View VTop5_PhoneSell
+as
+Select TOP(5)
+	PD.Id,
+	P.PhoneName,
+	PD.Price,
+	IMG.[Image],
+	COUNT(BD.Id) Total
+from Bill A
+Join BillDetails BD on BD.IdBill = A.Id
+join PhoneDetailds PD on PD.Id = Bd.IdPhoneDetail
+Join Phones P On P.Id = PD.IdPhone
+Left Join ListImage IMG on IMG.IdPhoneDetaild = PD.Id 
+WHERE PaymentDate > DATEADD(day, -80,  DATETRUNC( day,GETDATE())) and PaymentDate < GETDATE()
+group by PD.Id,P.PhoneName,PD.Price,IMG.[Image]
+order by Total DESC
